c383ff02a88143d0f0a95c584197da9aa165d2ad
HeydyCH <heidich8@gmail.com>
2020-02-01 09:53:08 -0500

    feat: add graficos variables numericas y categoricas

--- 3_Analisis_exploratorio_datos.R
+++ 3_Analisis_exploratorio_datos.R
@@ -20,3 +20,31 @@ heart_disease=heart_disease
 
 ## Análisis inicial de datos: df_status
 df_status(heart_disease)  ## Importante
+
+
+## Gráficos de distribución (VARIABLES NUMERICAS): plot_num
+plot_num(heart_disease)
+
+## Descripción cuantitativa (números...): summary, describe, profiling_num
+summary(heart_disease) ## Clasica R base
+describe(heart_disease) ## Paquete Hmisc con porcentaje
+profiling_num(heart_disease) ##  analisis mas exaustivo para variable categoricas
+
+
+
+
+## Gráficos de distribución (VARIABLES CATEGORICAS): freq
+freq(heart_disease)
+
+tbl=freq(heart_disease$chest_pain, path_out = "graficos") 
+## na.rm = True todos el analisis sin los nulos
+## path_out = "graficos" guarda los graficos en esa carpeta
+tbl
+
+
+
+
+
+
+
+
